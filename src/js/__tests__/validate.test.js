14b31c46b99c99c950d320ecd8ad8f355ab7390f
bochkarevatat <tanitabt@rambler.ru>
2023-08-04 20:12:25 +0400

    second commit

--- src/js/__tests__/validate.test.js
+++ src/js/__tests__/validate.test.js
@@ -3,32 +3,31 @@ import Timeline from '../timeline.js';
 const timeline = new Timeline();
 
 test('Valid with a space', () => {
-    const received = timeline.checkValidity('51.50851, -0.1257');
-    expect(received).toEqual(['51.50851, -0.1257']);
-  });
-  
-  test('Valid without spaces', () => {
-    const received = timeline.checkValidity('51.50851,0.1257');
-    expect(received).toEqual(['51.50851,0.1257']);
-  });
-  
-  test('Valid have square brackets', () => {
-    const received = timeline.checkValidity('[51.50851, -0.12572]');
-    expect(received).toEqual(['[51.50851, -0.12572]']);
-  });
-  
-  test('nothing entered', () => {
-    const received = timeline.checkValidity('');
-    expect(received).toBe(null);
-  });
-  
-  test('Wrong format', () => {
-    const received = timeline.checkValidity('675667,4883939');
-    expect(received).toBe(null);
-  });
-  
-  test('Not digital format', () => {
-    const received = timeline.checkValidity('dfgdh, 34.99393');
-    expect(received).toBe(null);
-  });
+  const received = timeline.checkValidity('51.50851, -0.1257');
+  expect(received).toEqual(['51.50851, -0.1257']);
+});
 
+test('Valid without spaces', () => {
+  const received = timeline.checkValidity('51.50851,0.1257');
+  expect(received).toEqual(['51.50851,0.1257']);
+});
+
+test('Valid have square brackets', () => {
+  const received = timeline.checkValidity('[51.50851, -0.12572]');
+  expect(received).toEqual(['[51.50851, -0.12572]']);
+});
+
+test('nothing entered', () => {
+  const received = timeline.checkValidity('');
+  expect(received).toBe(null);
+});
+
+test('Wrong format', () => {
+  const received = timeline.checkValidity('675667,4883939');
+  expect(received).toBe(null);
+});
+
+test('Not digital format', () => {
+  const received = timeline.checkValidity('dfgdh, 34.99393');
+  expect(received).toBe(null);
+});
--- src/js/timeline.js
+++ src/js/timeline.js
@@ -2,19 +2,17 @@
 /* eslint-disable no-alert */
 /* eslint-disable no-console */
 
-
 export default class Timeline {
-    constructor(parentEl) {
-      this.parentEl = parentEl;
-      this.message = null;
-      this.formInput = null;
-      this.modalInput = null;
-      this.addMessage = this.addMessage.bind(this);
-      
-    }
-  
-    static get markup() {
-      return `
+  constructor(parentEl) {
+    this.parentEl = parentEl;
+    this.message = null;
+    this.formInput = null;
+    this.modalInput = null;
+    this.addMessage = this.addMessage.bind(this);
+  }
+
+  static get markup() {
+    return `
       <div class="container-tl">
       <div class="list"></div>
       <div class="input-form">
@@ -26,84 +24,84 @@ export default class Timeline {
       </div>
       </div> 
       `;
-    }
+  }
 
-    static get containerTl() {
-      return '.container-tl';
-    }
+  static get containerTl() {
+    return '.container-tl';
+  }
 
-    static get list() {
-      return '.list';
-    }
+  static get list() {
+    return '.list';
+  }
 
-    static get formInput() {
-      return '.form-input';
-    }
+  static get formInput() {
+    return '.form-input';
+  }
 
-    static get form() {
-      return '.form';
-    }
-    
-    bindToDOM() {
-      this.parentEl.innerHTML = Timeline.markup;
-      this.containerTl = this.parentEl.querySelector(Timeline.containerTl);
-      this.list = this.parentEl.querySelector(Timeline.list);
-      this.form = this.parentEl.querySelector(Timeline.form);
-      this.formInput = this.parentEl.querySelector(Timeline.formInput);
-      this.form.addEventListener('submit', this.addMessage);
-    }
+  static get form() {
+    return '.form';
+  }
 
-    addMessage(event) {
-      event.preventDefault();
-      this.elem = document.createElement('div');
-      this.elem.classList.add('message');
-      this.list.appendChild(this.elem);
-      this.letter = document.createElement('div');
-      this.letter.classList.add('letter');
-      this.elem.appendChild(this.letter);
-      this.letter.textContent = event.target.input.value;
-      this.data = document.createElement('div');
-      this.data.classList.add('data');
-      this.data.textContent = `${new Date().toLocaleString()}`;
-      this.elem.appendChild(this.data);
-      this.location = document.createElement('div');
-      this.location.classList.add('location');
-      this.getLocation();
-      this.resetInput();
-    }
+  bindToDOM() {
+    this.parentEl.innerHTML = Timeline.markup;
+    this.containerTl = this.parentEl.querySelector(Timeline.containerTl);
+    this.list = this.parentEl.querySelector(Timeline.list);
+    this.form = this.parentEl.querySelector(Timeline.form);
+    this.formInput = this.parentEl.querySelector(Timeline.formInput);
+    this.form.addEventListener('submit', this.addMessage);
+  }
 
-    resetInput() {
-      this.formInput.value = '';
-    }
+  addMessage(event) {
+    event.preventDefault();
+    this.elem = document.createElement('div');
+    this.elem.classList.add('message');
+    this.list.appendChild(this.elem);
+    this.letter = document.createElement('div');
+    this.letter.classList.add('letter');
+    this.elem.appendChild(this.letter);
+    this.letter.textContent = event.target.input.value;
+    this.data = document.createElement('div');
+    this.data.classList.add('data');
+    this.data.textContent = `${new Date().toLocaleString()}`;
+    this.elem.appendChild(this.data);
+    this.location = document.createElement('div');
+    this.location.classList.add('location');
+    this.getLocation();
+    this.resetInput();
+  }
 
-    getLocation() {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            const { latitude, longitude } = position.coords;
-            this.showCoordinate(latitude.toFixed(4), longitude.toFixed(4));
-          },
-          (error) => {
-            this.showModal();
-            console.error(error);
-          },
-        );
-      }
-    }
+  resetInput() {
+    this.formInput.value = '';
+  }
 
-    showCoordinate(latitude, longitude) {
-      this.location.textContent = `[${latitude}, ${longitude}]`;
-      this.elem.insertAdjacentElement('beforeEnd', this.location);
-      this.locationIcon = document.createElement('div');
-      this.locationIcon.classList.add('locationIcon');
-      // this.elem.appendChild(this.locationIcon);
-      this.location.insertAdjacentElement('afterEnd', this.locationIcon);
+  getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          this.showCoordinate(latitude.toFixed(4), longitude.toFixed(4));
+        },
+        (error) => {
+          this.showModal();
+          console.error(error);
+        },
+      );
     }
+  }
 
-    showModal() {
-      const modal = document.createElement('div');
-      modal.classList.add('modal');
-      modal.innerHTML = `
+  showCoordinate(latitude, longitude) {
+    this.location.textContent = `[${latitude}, ${longitude}]`;
+    this.elem.insertAdjacentElement('beforeEnd', this.location);
+    this.locationIcon = document.createElement('div');
+    this.locationIcon.classList.add('locationIcon');
+    // this.elem.appendChild(this.locationIcon);
+    this.location.insertAdjacentElement('afterEnd', this.locationIcon);
+  }
+
+  showModal() {
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
+    modal.innerHTML = `
         <div class="modal-text">Что-то пошло не так  
         <p>Извините, но мы не можем определить ваше местоположение, 
         пожалуйста, дайте нам разрешение на использование геолокации, 
@@ -119,37 +117,37 @@ export default class Timeline {
         </form>
         </div>
       `;
-      this.parentEl.appendChild(modal);
-      const modalInput = this.parentEl.querySelector('.modal-input');
-      console.log(modalInput);
-      this.parentEl.querySelector('.modal-form').addEventListener('reset', (event) => {
-        event.preventDefault();
+    this.parentEl.appendChild(modal);
+    const modalInput = this.parentEl.querySelector('.modal-input');
+    console.log(modalInput);
+    this.parentEl.querySelector('.modal-form').addEventListener('reset', (event) => {
+      event.preventDefault();
+      this.hideModal();
+    });
+    this.parentEl.querySelector('.modal-form').addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      let value = this.checkValidity(event.target.modal.value);
+      console.log(value);
+      if (value) {
+        value = value[0].split(',');
+        const latitude = value[0].trim();
+        const longitude = value[1].trim();
+
         this.hideModal();
-      });
-      this.parentEl.querySelector('.modal-form').addEventListener('submit', (event) => {
-        event.preventDefault();
+        this.showCoordinate(latitude, longitude);
+      } else {
+        alert('Неверный формат данных');
+      }
+    });
+  }
 
-        let value = this.checkValidity(event.target.modal.value);
-        console.log(value);
-        if (value) {
-          value = value[0].split(',');
-          const latitude = value[0].trim();
-          const longitude = value[1].trim();
-
-          this.hideModal();
-          this.showCoordinate(latitude, longitude);
-        } else {
-          alert('Неверный формат данных');
-        }
-      });
-    }
-
-    hideModal() {
-      this.parentEl.querySelector('.modal').remove();
-      this.resetInput();
-    }
-
-    checkValidity(string) {
-      return string.match(/^\[?\d+\.\d+,\s?\-?\d+\.\d+\]?$/gm);
-    }
+  hideModal() {
+    this.parentEl.querySelector('.modal').remove();
+    this.resetInput();
+  }
+/* eslint-disable */
+  checkValidity(string) {
+    return string.match(/^\[?\d+\.\d+,\s?\-?\d+\.\d+\]?$/gm);
+  }
 }
